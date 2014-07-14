9e2861f9f2816c01d587dda98ede4e11f4cd681a
Nick DeNardis <nick.denardis@gmail.com>
2014-07-14 16:17:09 -0400

    Updated the html presentations.

--- lessons/class2/practice/search.php
+++ lessons/class2/practice/search.php
@@ -1,32 +1,12 @@
 <?php
-
-// Include the famous women
-include_once 'includes/women.php';
-
-$results = array();
-
-// If trying to search
-if ( isset($_GET['q']) && ctype_alnum($_GET['q']) )
-{
-    $search_term = filter_input(INPUT_GET, 'q', FILTER_SANITIZE_SPECIAL_CHARS);
-
-    if ( is_array($women) )
+    if (isset($_GET['q']))
     {
-      foreach ($women as $woman)
-      {
-          if ( stristr($women['name'], $search_term) )
-          {
-            $results[] = $women;
-          }
-      }
+        $search_term = filter_input(INPUT_GET, 'q', FILTER_SANITIZE_SPECIAL_CHARS);
     }
-}
 ?>
-<?php
-require_once 'includes/top.php';
+<?php require_once 'includes/top.php'; ?>
 
-include_once 'includes/header.php';
-?>
+<?php include_once 'includes/header.php'; ?>
 
     <div id="main">
         <section id="search">
@@ -39,18 +19,9 @@ include_once 'includes/header.php';
                 <input type="submit" value="Search" />
             </form>
         </section>
-        <?php if ( isset($search_term) ) { ?>
-          <section class="results">
-              <p>Results for: <?php echo $search_term; ?></p>
-
-              <?php
-              foreach ($results as $woman)
-              {
-                echo viewWoman($woman['name'], $woman['link'], $woman['years'], $woman['image'], $woman['biography']);
-              }
-              ?>
-          </section>
-        <?php } ?>
+        <section class="results">
+            <?php echo $_GET['q']; ?>
+        </section>
     </div>
 
 <?php include_once 'includes/bottom.php'; ?>
